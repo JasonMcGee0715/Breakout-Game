c0adc3901d227af6c9fdd818a7d580cbc5e8c50c
JasonMcGee0715 <jdm0715@gmail.com>
2020-06-07 22:34:50 -0500

    added items js

---
+++ script.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+function draw() {
+    // drawing code
+}
+setInterval(draw, 10);
+
+ctx.beginPath();
+ctx.arc(50, 50, 10, 0, Math.PI*2);
+ctx.fillStyle = "#0095DD";
+ctx.fill();
+ctx.closePath();
